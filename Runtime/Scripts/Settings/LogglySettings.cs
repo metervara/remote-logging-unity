8597769ae89303a9303a5657c5c8547c86dcc61c
Patrik Svensson <ps@molamil.com>
2021-07-05 13:36:01 +0200

    chore: initial project

---
+++ Runtime/Scripts/Settings/LogglySettings.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace metervara.logging
+{
+  [System.Serializable]
+  public class LogglySettings
+  {
+    public string TAG = "Unity3D";
+    public string apiToken = "";
+  }
+}
